596f928d63cc9d7b35288d710bee7906bd1ea43a
Louis Ruocco <louisruocco1@gmail.com>
2022-01-27 18:13:52 +0000

    server setup done

--- server.js
+++ server.js
@@ -1,6 +1,13 @@
 const express = require("express");
 const dotenv = require("dotenv");
+const ejs = require("ejs");
+const flash = require("connect-flash");
 const app = express();
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log("Listening at " + port));
+
+app.use(express.json());
+app.use(express.urlencoded({extended: false}));
+app.set("view engine", "ejs");
+app.use(flash());
