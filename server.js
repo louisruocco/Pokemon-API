c4590a7270a7ab559ba4df4736e2a70e93793eea
Louis Ruocco <louisruocco1@gmail.com>
2022-01-27 18:41:14 +0000

    connected to db

--- server.js
+++ server.js
@@ -1,8 +1,11 @@
 const express = require("express");
 const dotenv = require("dotenv");
+const db = require("./db");
 const ejs = require("ejs");
 const flash = require("connect-flash");
 const app = express();
+
+dotenv.config({path: "./.env"});
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log("Listening at " + port));
