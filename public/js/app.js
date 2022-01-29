ba9dfdb40fdc7d767b20b9fcf57ccdfae140ec51
Louis Ruocco <louisruocco1@gmail.com>
2022-01-29 22:50:56 +0000

    added for loop to loop through pokemon and add to pokedex as they are added to the db

--- public/js/app.js
+++ public/js/app.js
@@ -4,9 +4,10 @@ window.addEventListener("load", async () => {
     const url = "http://localhost:3000/pokemon";
     const response = await fetch(url);
     const data = await response.json();
-    // console.log(data)
-    
-    const name = document.createElement("a");
-    name.textContent = data[0].name;
-    pokemonDiv.appendChild(name);
+      
+    data.forEach(pokemon => {
+        const name = document.createElement("a");
+        name.textContent = pokemon.name;
+        pokemonDiv.appendChild(name);
+    })
 })
