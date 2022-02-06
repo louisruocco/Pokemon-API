654efd8d9408019631c87cb50ae6d6a7b11f76d6
Louis Ruocco <louisruocco1@gmail.com>
2022-02-06 16:22:08 +0000

    ready for deployment

--- server.js
+++ server.js
@@ -2,10 +2,6 @@ const express = require("express");
 const dotenv = require("dotenv");
 const db = require("./db");
 const ejs = require("ejs");
-<<<<<<< HEAD
-=======
-const flash = require("connect-flash");
->>>>>>> 9433a1c2ff4f326f00c0dbaabab56542b2ec5caa
 const app = express();
 
 dotenv.config({path: "./.env"});
@@ -16,10 +12,6 @@ app.listen(port, () => console.log("Listening at " + port));
 app.use(express.json());
 app.use(express.urlencoded({extended: false}));
 app.set("view engine", "ejs");
-<<<<<<< HEAD
-=======
-app.use(flash());
->>>>>>> 9433a1c2ff4f326f00c0dbaabab56542b2ec5caa
 app.use(express.static("public"));
 app.use("/css", express.static(__dirname + "public/css"));
 app.use("/js", express.static(__dirname + "public/js"));
